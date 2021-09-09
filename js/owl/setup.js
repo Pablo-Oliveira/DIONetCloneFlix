07a286d513e0b2f19badfa3f1249e290f0de5d54
Pablo Oliveira <pablue@gmail.com>
2021-09-09 01:43:09 -0400

    Versão satírica

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -16,3 +16,18 @@ $('.owl-carousel').owlCarousel({
 })
 
 //meu código
+window.onload = function () {
+
+    // Video
+    var video = document.getElementById("video");
+
+    // Buttons
+    var playButton = document.getElementById("play-pause");
+    var muteButton = document.getElementById("mute");
+    var fullScreenButton = document.getElementById("full-screen");
+
+    // Sliders
+    var seekBar = document.getElementById("seek-bar");
+    var volumeBar = document.getElementById("volume-bar");
+
+}
